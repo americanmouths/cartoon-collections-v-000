c43c634cd6c7dfc8393ce4ce0d4c2d69a0cca8b2
Kayla Hansen <shiverr@gmail.com>
2017-10-13 03:07:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |name, index|
-    puts "#{index} #{name}"
+  dwarves.each_with_index(1) do |name, index|
+    puts "#{index}. #{name}"
   end
 end
 
