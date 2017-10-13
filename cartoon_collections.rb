792c325f07458f88669238788fd0253de6a1d9da
Kayla Hansen <shiverr@gmail.com>
2017-10-13 03:37:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,12 +7,11 @@ end
 def summon_captain_planet(planeteer_calls)
     planeteer_calls.map! do |name|
       name.capitalize << "!"
-
     end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|i| i.length >= 4}
 end
 
 def find_the_cheese# code an argument here
